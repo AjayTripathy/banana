9a7310d5bdd9c6d19c3afd153e18e9e36684f11b
Ajay Tripathy <aurotripathy@auro-tripathys-MacBook-Pro.local>
2012-09-09 22:33:17 -0700

    yaaay facebook auth

--- server.js
+++ server.js
@@ -6,13 +6,24 @@ var db = mongo.db('localhost:27017/banana');
 var nodemailer = require('nodemailer');
 var rest = require('restler');
 
+var passport = require('passport') , FacebookStrategy = require('passport-facebook').Strategy;
+
+var Users = db.collection('user');
+
+
+passport.use(new FacebookStrategy({
+    clientID: '254514314669541',
+    clientSecret: '6b5be69ef3216d9908838c0477b0f503',
+    callbackURL: 'home'
+  },
+  function(accessToken, refreshToken, profile, done){
+    console.log('callback called');
+  }
+));
 
 var app =  express.createServer();
 
-var familyStatus = db.collection('familyStatus');
 
-var crypto = require('crypto');
-var hash = require('node_hash');
 
 app.use(express.static(__dirname + '/static'));
 app.use(express.bodyParser());
@@ -24,8 +35,16 @@ app.set('view options', {
       layout: false
 });
 
+app.get('/auth' , passport.authenticate('facebook')  )
+app.get('/home' , function(req, res){
+res.send('hi');
+
+})
 app.get('/' , function(req, res){
-  console.log("hello")
+  res.send('cheese');
+});
+app.get('failure', function(req, res){
+  console.log('you done goofed')
 })
 
-app.listen(8080);
+app.listen(80);
